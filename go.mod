ab4d36f601661a351114ef6d5fcee8b02397c4d0
jsalinlee <jsalinlee@gmail.com>
2023-05-19 11:49:09 -0700

    persistence is key

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module practicemodule
 
 go 1.20
+
+require github.com/jsalinlee/puppy v1.3.1
+
+require github.com/jsalinlee/dog v0.0.0-20230510183540-c78e4d417ce1 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/jsalinlee/puppy"
 )
 
 func main() {
-	fmt.Println("Practicing tagging versions")
+	puppy.From13()
+
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+
+	s3 := puppy.BigBark()
+	s4 := puppy.BigBarks()
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+	fmt.Println(s3)
+	fmt.Println(s4)
 }
